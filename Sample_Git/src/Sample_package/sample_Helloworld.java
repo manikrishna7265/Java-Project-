72b2916c70ce55cdf62bee9bbaf72fe80a2669da
ibm <ibm@MH62.corp.miraclesoft.com>
2023-02-17 20:52:36 +0530

    Bookstore project

---
+++ Sample_Git/src/Sample_package/sample_Helloworld.java
@@ -0,0 +1,10 @@
+package Sample_package;
+
+public class sample_Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
---
+++ Sample_Git/src/module-info.java
@@ -0,0 +1,2 @@
+module Sample_Git {
+}
